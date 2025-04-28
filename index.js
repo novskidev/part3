00f62a774e58668833a6606bff76f8a84a3baad4
Novian <razecoding@gmail.com>
2025-04-28 09:56:29 +0700

    3.5: Phonebook backend step 5

--- index.js
+++ index.js
@@ -53,6 +53,26 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const newId = Math.floor(Math.random() * 1000000); // Generate a random ID
+  const person = {
+    id: newId.toString(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
